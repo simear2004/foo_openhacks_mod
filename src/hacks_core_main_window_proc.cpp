1ad20460f3b4be4a1ac27d1a07d3753b0af2dc8b
YeYo <16457576+simear2004@users.noreply.github.com>
2026-04-07 10:45:57 +0800

    Update hacks_core_main_window_proc.cpp

--- src/hacks_core_main_window_proc.cpp
+++ src/hacks_core_main_window_proc.cpp
@@ -124,10 +124,11 @@ LRESULT OpenHacksCore::OpenHacksMainWindowProc(HWND wnd, UINT msg, WPARAM wp, LP
         break;
 
     case WM_NCACTIVATE:
-        // if (OpenHacksVars::MainWindowFrameStyle == WindowFrameStyleNoBorder)
-        //     return CallWindowProc(mMainWindowOriginProc, wnd, msg, wp, -1);
-        // break;
+        if (OpenHacksVars::MainWindowFrameStyle == WindowFrameStyleNoBorder)
+            return CallWindowProc(mMainWindowOriginProc, wnd, msg, wp, -1);
+        break;
 
+    case WM_NCCALCSIZE:
         if (OpenHacksVars::MainWindowFrameStyle == WindowFrameStyleNoBorder && wp == TRUE)
         {
             LPNCCALCSIZE_PARAMS params = reinterpret_cast<LPNCCALCSIZE_PARAMS>(lp);
