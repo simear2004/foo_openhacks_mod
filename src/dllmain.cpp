43b1fc2f44ea5df81d9980df61bc828db02787d8
YeYo <16457576+simear2004@users.noreply.github.com>
2026-04-10 18:07:15 +0800

    Update dllmain.cpp

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -3,6 +3,8 @@
 #include "hacks_version.h"
 #include <libPPUI/Controls.h>
 #include "hacks_com_impl.h"
+#include <windows.h>
+#include <string>
 
 BOOL APIENTRY DllMain(HMODULE mod, DWORD reason, LPVOID /*reserved*/)
 {
@@ -10,6 +12,20 @@ BOOL APIENTRY DllMain(HMODULE mod, DWORD reason, LPVOID /*reserved*/)
     {
     case DLL_PROCESS_ATTACH:
         DisableThreadLibraryCalls(mod);
+        
+        {
+            char exePath[MAX_PATH] = {0};
+            if (GetModuleFileNameA(NULL, exePath, MAX_PATH)) {
+                std::string path(exePath);
+                size_t pos = path.find_last_of('\\');
+                if (pos != std::string::npos) {
+                    std::string root = path.substr(0, pos);
+                    SetEnvironmentVariableA("fb2k", root.c_str());
+                    SetEnvironmentVariableA("foobar2000", root.c_str());
+                }
+            }
+        }
+
         CSeparator::Register();
         // Initialize COM automation server
         if (SUCCEEDED(OleInitialize(nullptr)))
