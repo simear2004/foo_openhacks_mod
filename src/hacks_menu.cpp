adf6ebd22869aa18a98a588188625e9e20191739
YeYo <16457576+simear2004@users.noreply.github.com>
2026-04-13 13:39:27 +0800

    Translation: Chinese Simplified Language

---
+++ src/hacks_menu.cpp
@@ -0,0 +1,76 @@
+﻿#include "pch.h"
+#include "hacks_menu.h"
+
+OpenHacksMenu& OpenHacksMenu::Get()
+{
+    static OpenHacksMenu menu;
+    return menu;
+}
+
+HMENU OpenHacksMenu::GenerateMenu()
+{
+    if (HMENU menu = CreatePopupMenu())
+    {
+        GenerateMenuUsingManager(menu, mFileMenuPtr, mainmenu_groups::file, L"文件", CommandFileStart);
+        GenerateMenuUsingManager(menu, mEditMenuPtr, mainmenu_groups::edit, L"编辑", CommandEditStart);
+        GenerateMenuUsingManager(menu, mViewMenuPtr, mainmenu_groups::view, L"视图", CommandViewStart);
+        GenerateMenuUsingManager(menu, mPlaybackMenuPtr, mainmenu_groups::playback, L"播放", CommandPlaybackStart);
+        GenerateMenuUsingManager(menu, mLibraryMenuPtr, mainmenu_groups::library, L"媒体库", CommandLibraryStart);
+        GenerateMenuUsingManager(menu, mHelpMenuPtr, mainmenu_groups::help, L"帮助", CommandHelpStart);
+
+        return menu;
+    }
+
+    return nullptr;
+}
+
+void OpenHacksMenu::ExecuteMenuCommand(int32_t cmd)
+{
+    auto InBounds = [cmd](const int32_t start) { return (cmd >= start) && (cmd < (start + CommandRootMenuMaxCount)); };
+
+    if (InBounds(CommandFileStart))
+    {
+        mFileMenuPtr->execute_command(cmd - CommandFileStart);
+    }
+    else if (InBounds(CommandViewStart))
+    {
+        mViewMenuPtr->execute_command(cmd - CommandViewStart);
+    }
+    else if (InBounds(CommandEditStart))
+    {
+        mEditMenuPtr->execute_command(cmd - CommandEditStart);
+    }
+    else if (InBounds(CommandPlaybackStart))
+    {
+        mPlaybackMenuPtr->execute_command(cmd - CommandPlaybackStart);
+    }
+    else if (InBounds(CommandLibraryStart))
+    {
+        mLibraryMenuPtr->execute_command(cmd - CommandLibraryStart);
+    }
+    else if (InBounds(CommandHelpStart))
+    {
+        mHelpMenuPtr->execute_command(cmd - CommandHelpStart);
+    }
+}
+
+void OpenHacksMenu::GenerateMenuUsingManager(HMENU menu, mainmenu_manager::ptr& manager, const GUID& guid, wstring_view_t caption, uint32_t baseId)
+{
+    if (manager.is_empty())
+    {
+        if (manager = mainmenu_manager::get(); manager.is_valid())
+        {
+            manager->instantiate(guid);
+        }
+    }
+
+    if (manager.is_empty())
+        return;
+
+    if (HMENU subMenu = CreatePopupMenu())
+    {
+        const t_uint32 flags = mainmenu_manager::flag_show_shortcuts;
+        manager->generate_menu_win32(subMenu, baseId, CommandRootMenuMaxCount, flags);
+        AppendMenuW(menu, MF_POPUP, (UINT_PTR)subMenu, caption.data());
+    }
+}
---
+++ src/hacks_version.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include "hacks_version.h"
+
+static string_view_t OpenHacksFormatAbort = R"OH(OpenHacksMod v%s for foobar2000
+author: ohyeah
+Mod: YeYo (汉化: 智享阁)
+
+MSVC: v%s %s
+Compile: %s
+Foobar2000 SDK: %s
+
+Third-party libraries:
+
+MinHook - The Minimalistic API Hooking Library for x64/x86
+Copyright (C) 2009-2017 Tsuda Kageyu.
+All rights reserved.
+
+
+)OH";
+
+const char* GetOpenHacksAbout()
+{
+    static char about[8 * 1024] = {0};
+    if (about[0] == '\0')
+    {
+        char ver[64] = {0};
+        unsigned major = _MSC_FULL_VER / 10000000;
+        unsigned minor = _MSC_FULL_VER / 100000 - major * 100;
+        unsigned build = _MSC_FULL_VER - major * 10000000 - minor * 100000;
+        sprintf_s(ver, "%d.%d.%d.%02d", major, minor, build, _MSC_BUILD);
+        sprintf_s(about, OpenHacksFormatAbort.data(), HACKS_VERSION, ver, STRINGIFY(_MSVC_LANG), "" __TIME__ " " __DATE__, STRINGIFY(FOOBAR2000_SDK_VERSION));
+    }
+
+    return about;
+}
