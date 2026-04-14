b4b6bb9b2012002ee72664961f79ef11b69a8c61
YeYo <16457576+simear2004@users.noreply.github.com>
2026-04-14 09:49:06 +0800

    Update hacks_core.cpp

--- src/hacks_core.cpp
+++ src/hacks_core.cpp
@@ -78,7 +78,7 @@ void OpenHacksCore::Initialize()
         if (mUsedCompositedStyle)
         {
             LONG exStyle = GetWindowLong(window, GWL_EXSTYLE);
-            SetWindowLong(window, GWL_EXSTYLE, exStyle & ~WS_EX_COMPOSITED);
+            SetWindowLong(window, GWL_EXSTYLE, exStyle & ~(WS_EX_COMPOSITED | WS_EX_LAYERED));
             mUsedCompositedStyle = false;
             
             RedrawWindow(window, nullptr, nullptr, RDW_INVALIDATE | RDW_UPDATENOW | RDW_ALLCHILDREN);
