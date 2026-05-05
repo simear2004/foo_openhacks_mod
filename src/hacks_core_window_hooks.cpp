7a9e4f03b99647d2acd2a550bc7ab183f16ba30d
YeYo <16457576+simear2004@users.noreply.github.com>
2026-05-05 20:44:11 +0800

    Fix: Clear background color when resizing window

--- src/hacks_core_window_hooks.cpp
+++ src/hacks_core_window_hooks.cpp
@@ -105,6 +105,8 @@ LRESULT OpenHacksCore::OpenHacksCallWndProc(int code, WPARAM wp, LPARAM lp)
                 if (className == kDUIMainWindowClassName)
                 {
                     mMainWindow = pcwps->hwnd;
+
+                    SetClassLongPtr(pcwps->hwnd, GCLP_HBRBACKGROUND, (LONG_PTR)GetStockObject(NULL_BRUSH));
                     
                     LONG exStyle = GetWindowLong(pcwps->hwnd, GWL_EXSTYLE);
                     SetWindowLong(pcwps->hwnd, GWL_EXSTYLE, exStyle | WS_EX_COMPOSITED | WS_EX_LAYERED);
