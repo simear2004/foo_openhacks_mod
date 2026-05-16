78c87b671763715a9952cf5d90bbe89cb01efad6
YeYo <16457576+simear2004@users.noreply.github.com>
2026-05-16 10:28:36 +0800

    Fix: The problem of displaying under special conditions when the menu bar is set to hidden

--- src/hacks_core_main_window_proc.cpp
+++ src/hacks_core_main_window_proc.cpp
@@ -178,7 +178,29 @@ LRESULT OpenHacksCore::OpenHacksMainWindowProc(HWND wnd, UINT msg, WPARAM wp, LP
     case WM_SIZE:
         if (OnSize(wnd, wp, lp))
             return 0;
+        if (mRebarWindow && mMainMenuWindow)
+        {
+            PostMessage(wnd, WM_USER + 1001, 0, 0);
+        }
         break;
+
+    case WM_ACTIVATE:
+        if (LOWORD(wp) != WA_INACTIVE && mRebarWindow && mMainMenuWindow)
+        {
+            PostMessage(wnd, WM_USER + 1001, 0, 0);
+        }
+        break;
+
+    case WM_USER + 1001:
+    {
+        bool shouldShow = OpenHacksVars::ShowMainMenu;
+        bool isShowing = IsMenuBarVisible();
+        if (shouldShow != isShowing)
+        {
+            ShowOrHideMenuBar(shouldShow);
+        }
+        return 0;
+    }
 
     case WM_DPICHANGED: // fixme: won't receive currently(DPI System aware).
         OpenHacksVars::DPI = static_cast<uint32_t>(LOWORD(wp));
